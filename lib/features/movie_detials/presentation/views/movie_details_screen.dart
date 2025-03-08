0a659cc32b2d8dd289020216fb5201082f8aacd8
MohamedElsay0d <me827794@gmail.com>
2025-03-08 04:35:48 +0200

    finsih part of ui and logic of detials screen

--- lib/features/movie_detials/presentation/views/movie_details_screen.dart
+++ lib/features/movie_detials/presentation/views/movie_details_screen.dart
@@ -5,9 +5,9 @@ import 'package:movies_app/core/Themes/colors.dart';
 import 'package:movies_app/core/shared/widgets/custom_elevated_button.dart';
 import 'package:movies_app/core/utils/helper/helper_functions.dart';
 import 'package:movies_app/features/home/data/model/movie_response.dart';
+import 'package:movies_app/features/home/presentation/widgets/custom_card.dart';
 import 'package:movies_app/features/movie_detials/presentation/widgets/info_card.dart';
 import 'package:movies_app/features/movie_detials/presentation/widgets/screenShot_item.dart';
-import 'package:movies_app/features/onboarding/widgets/custom_outlined_button.dart';
 import 'package:shimmer/shimmer.dart';
 
 class MovieDetailsScreen extends StatelessWidget {
@@ -210,194 +210,52 @@ class MovieDetailsScreen extends StatelessWidget {
           screenShot_item(
             imgeUrl: movie.mediumCoverImage,
           ),
+          SizedBox(height: 20.h),
+          // Similar Movies
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Similar Movies',
+                style: HelperFunction.textTheme(context).titleLarge,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0.w),
+            child: GridView.count(
+              crossAxisCount: 2,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              mainAxisSpacing: 15.h,
+              crossAxisSpacing: 12.w,
+              children: [
+                CustomCard(
+                  customWidth: 189.w,
+                  customHeight: 279.h,
+                  movie: movie,
+                ),
+                CustomCard(
+                  customWidth: 189.w,
+                  customHeight: 279.h,
+                  movie: movie,
+                ),
+                CustomCard(
+                  customWidth: 189.w,
+                  customHeight: 279.h,
+                  movie: movie,
+                ),
+                CustomCard(
+                  customWidth: 189.w,
+                  customHeight: 279.h,
+                  movie: movie,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     ));
   }
 }
-
-
-
-
-
-
-//  Stack(
-//         children: [
-//           // Background Image with Gradient Overlay
-//           Container(
-//             height: MediaQuery.of(context).size.height,
-//             width: MediaQuery.of(context).size.width,
-//             decoration: BoxDecoration(
-//               image: DecorationImage(
-//                 image: CachedNetworkImageProvider(
-//                   movie.largeCoverImage,
-//                 ),
-//                 fit: BoxFit.fill,
-//               ),
-//             ),
-//             child: Container(
-//               decoration: BoxDecoration(
-//                 gradient: LinearGradient(
-//                   begin: Alignment.topCenter,
-//                   end: Alignment.bottomCenter,
-//                   colors: [
-//                     ThemeColors.black.withOpacity(0.4),
-//                     ThemeColors.black.withOpacity(0.6),
-//                     ThemeColors.black.withOpacity(0.8),
-//                     ThemeColors.black,
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//           Center(
-//             child: Container(
-//               margin: EdgeInsets.only(bottom: 200.h),
-//               width: 70.w,
-//               height: 70.h,
-//               decoration: BoxDecoration(
-//                 color: Colors.amber,
-//                 shape: BoxShape.circle,
-//                 border: Border.all(color: Colors.white, width: 3),
-//               ),
-//               child: const Icon(
-//                 Icons.play_arrow,
-//                 size: 40,
-//                 color: Colors.white,
-//               ),
-//             ),
-//           ),
-//           // Content
-//           SafeArea(
-//             child: Padding(
-//               padding: EdgeInsets.symmetric(horizontal: 16.w),
-//               child: Column(
-//                 children: [
-//                   // App Bar
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(horizontal: 16.0),
-//                     child: Row(
-//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                       children: [
-//                         Container(
-//                           width: 40.w,
-//                           height: 40.h,
-//                           decoration: BoxDecoration(
-//                             color: ThemeColors.black.withOpacity(0.5),
-//                             borderRadius: BorderRadius.circular(12),
-//                           ),
-//                           child: IconButton(
-//                             icon: const Icon(Icons.arrow_back_ios_new),
-//                             color: ThemeColors.white,
-//                             onPressed: () {
-//                               Navigator.pop(context);
-//                             },
-//                           ),
-//                         ),
-//                         Container(
-//                           width: 40.w,
-//                           height: 40.h,
-//                           decoration: BoxDecoration(
-//                             color: ThemeColors.black.withOpacity(0.5),
-//                             borderRadius: BorderRadius.circular(12),
-//                           ),
-                          // child: IconButton(
-                          //   icon: const Icon(Icons.bookmark_border),
-                          //   color: ThemeColors.white,
-                          //   onPressed: () {
-                          //     // Handle bookmark action here
-                          //   },
-                          // ),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-
-//                   // Spacer for main content
-//                   const Spacer(),
-
-//                   // Movie Title
-                  // Text(
-                  //   movie.title,
-                  //   style: HelperFunction.textTheme(context).titleLarge,
-                  // ),
-//                   const SizedBox(height: 10),
-
-//                   // Year
-//                   const Text(
-//                     "2022",
-//                     style: TextStyle(
-//                       fontSize: 16,
-//                       color: Colors.grey,
-//                     ),
-//                   ),
-
-//                   const SizedBox(height: 30),
-
-//                   // Watch Button
-//                   CustomElevatedButton(
-//                     onPressed: () {
-//                       // Handle watch action here
-//                     },
-//                     buttonStyle: ElevatedButton.styleFrom(
-//                       minimumSize: Size(double.infinity, 55.h),
-//                       backgroundColor: ThemeColors.red,
-//                       foregroundColor: ThemeColors.white,
-//                     ),
-//                     child: const Text('Watch'),
-//                   ),
-
-//                   const SizedBox(height: 20),
-
-//                   // Ratings Row
-//                   const Row(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     children: [
-//                       // Likes
-//                       Row(
-//                         children: [
-//                           Icon(Icons.favorite, color: Colors.amber, size: 22),
-//                           SizedBox(width: 8),
-//                           Text("15",
-//                               style:
-//                                   TextStyle(fontSize: 16, color: Colors.white)),
-//                         ],
-//                       ),
-
-//                       SizedBox(width: 30),
-
-//                       // Duration
-//                       Row(
-//                         children: [
-//                           Icon(Icons.access_time,
-//                               color: Colors.amber, size: 22),
-//                           SizedBox(width: 8),
-//                           Text("90",
-//                               style:
-//                                   TextStyle(fontSize: 16, color: Colors.white)),
-//                         ],
-//                       ),
-
-//                       SizedBox(width: 30),
-
-//                       // Rating
-//                       Row(
-//                         children: [
-//                           Icon(Icons.star, color: Colors.amber, size: 22),
-//                           SizedBox(width: 8),
-//                           Text("7.6",
-//                               style:
-//                                   TextStyle(fontSize: 16, color: Colors.white)),
-//                         ],
-//                       ),
-//                     ],
-//                   ),
-
-//                   const SizedBox(height: 20),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-    
