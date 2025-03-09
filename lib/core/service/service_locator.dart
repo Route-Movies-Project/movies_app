58064e09f1fc67517aaaccd35b06435436ca427b
codingking02 <zeyadismailo2002@gmail.com>
2025-03-09 17:06:09 +0400

    added injecatble

--- lib/core/service/service_locator.dart
+++ lib/core/service/service_locator.dart
@@ -1,43 +1,7 @@
 import 'package:get_it/get_it.dart';
-import 'package:movies_app/features/auth/cubit/auth_cubit.dart';
-import 'package:movies_app/features/auth/data/data_source/auth_api_data_source.dart';
-import 'package:movies_app/features/auth/data/data_source/auth_shared_prefs_data_source.dart';
-import 'package:movies_app/features/auth/repositories/auth_repositorie.dart';
-import 'package:movies_app/features/home/cubit/movies_cubit.dart';
-import 'package:movies_app/features/home/cubit/movies_genre_cubit.dart';
-import 'package:movies_app/features/home/data/data_source/movies_api_data_source.dart';
-import 'package:movies_app/features/home/repository/movies_repository.dart';
-import 'package:movies_app/features/profile/cubit/profile_cubit.dart';
-import 'package:movies_app/features/profile/data/data_source/profile_api_data_source.dart';
-import 'package:movies_app/features/profile/repository/profile_repository.dart';
+import 'package:injectable/injectable.dart';
+import 'package:movies_app/core/service/service_locator.config.dart';
 
 final GetIt getIt = GetIt.instance;
-
-void setupLocator() {
-  getIt.registerSingleton<AuthCubit>(
-    AuthCubit(
-      AuthRepositorie(AuthApiDataSource(), AuthSharedPrefsDataSource()),
-    ),
-  );
-  getIt.registerSingleton<ProfileCubit>(
-    ProfileCubit(
-      ProfileRepository(
-        ProfileApiDataSource(),
-      ),
-    ),
-  );
-  getIt.registerSingleton(
-    MoviesCubit(
-      MoviesRepository(
-        MoviesApiDataSource(),
-      ),
-    ),
-  );
-  getIt.registerSingleton(
-    MoviesGenreCubit(
-      MoviesRepository(
-        MoviesApiDataSource(),
-      ),
-    ),
-  );
-}
+@InjectableInit()
+void setupLocator() => getIt.init();
