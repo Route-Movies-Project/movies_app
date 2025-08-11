d08a1b4ac0bfa5d0e736f489e6e5178a0ea61b60
codingking02 <zeyadismailo2002@gmail.com>
2025-08-11 11:29:30 +0300

    feat: added new screen

---
+++ lib/features/github_test/view/github_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GithubTest extends StatelessWidget {
+  const GithubTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(""),
+    );
+  }
+}
